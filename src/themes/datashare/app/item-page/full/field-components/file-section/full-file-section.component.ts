8e3816d4d6e3e021a24f2ba43e1626ae0043f0f3
John Pinto <jpinto@newington.edina.ac.uk>
2024-10-25 11:06:37 +0100

    Added CC-License bundle section to full item page.

--- src/themes/datashare/app/item-page/full/field-components/file-section/full-file-section.component.ts
+++ src/themes/datashare/app/item-page/full/field-components/file-section/full-file-section.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { Observable, switchMap, tap } from 'rxjs';
 
 import { FullFileSectionComponent as BaseComponent } from '../../../../../../../app/item-page/full/field-components/file-section/full-file-section.component';
 import { ThemedFileDownloadLinkComponent } from '../../../../../../../app/shared/file-download-link/themed-file-download-link.component';
@@ -13,13 +14,20 @@ import { PaginationComponent } from '../../../../../../../app/shared/pagination/
 import { FileSizePipe } from '../../../../../../../app/shared/utils/file-size-pipe';
 import { VarDirective } from '../../../../../../../app/shared/utils/var.directive';
 import { ThemedThumbnailComponent } from '../../../../../../../app/thumbnail/themed-thumbnail.component';
+import { PaginatedList } from '../../../../../../../app/core/data/paginated-list.model';
+import { RemoteData } from '../../../../../../../app/core/data/remote-data';
+import { Bitstream } from '../../../../../../../app/core/shared/bitstream.model';
+import { PaginationComponentOptions } from '../../../../../../../app/shared/pagination/pagination-component-options.model';
+import { followLink } from '../../../../../../../app/shared/utils/follow-link-config.model';
+import { hasValue } from '../../../../../../../app/shared/empty.util';
+
 
 @Component({
   selector: 'ds-themed-item-page-full-file-section',
   // styleUrls: ['./full-file-section.component.scss'],
   styleUrls: ['../../../../../../../app/item-page/full/field-components/file-section/full-file-section.component.scss'],
-  // templateUrl: './full-file-section.component.html',
-  templateUrl: '../../../../../../../app/item-page/full/field-components/file-section/full-file-section.component.html',
+  templateUrl: './full-file-section.component.html',
+  // templateUrl: '../../../../../../../app/item-page/full/field-components/file-section/full-file-section.component.html',
   standalone: true,
   imports: [
     PaginationComponent,
@@ -35,4 +43,27 @@ import { ThemedThumbnailComponent } from '../../../../../../../app/thumbnail/the
   ],
 })
 export class FullFileSectionComponent extends BaseComponent {
+  cclicenses$: Observable<RemoteData<PaginatedList<Bitstream>>>;
+
+  initialize(): void {
+    super.initialize();
+    this.cclicenses$ = this.paginationService.getCurrentPagination(this.licenseOptions.id, this.licenseOptions).pipe(
+      switchMap((options: PaginationComponentOptions) => this.bitstreamDataService.findAllByItemAndBundleName(
+        this.item,
+        'CC-LICENSE',
+        { elementsPerPage: options.pageSize, currentPage: options.currentPage },
+        true,
+        true,
+        followLink('format'),
+        followLink('thumbnail'),
+      )),
+      tap((rd: RemoteData<PaginatedList<Bitstream>>) => {
+        if (hasValue(rd.errorMessage)) {
+          this.notificationsService.error(this.translateService.get('file-section.error.header'), `${rd.statusCode} ${rd.errorMessage}`);
+        }
+      },
+      ),
+    );
+  }
+
 }
