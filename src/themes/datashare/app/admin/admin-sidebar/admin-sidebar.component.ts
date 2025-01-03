a51da18f15375952cdf2146968a1c38819eafb19
John Pinto <jpinto@newington.edina.ac.uk>
2025-01-03 15:03:52 +0000

    Replced dspace images with datashare specific images in admin-sidebar and login-page.

--- src/themes/datashare/app/admin/admin-sidebar/admin-sidebar.component.ts
+++ src/themes/datashare/app/admin/admin-sidebar/admin-sidebar.component.ts
@@ -16,10 +16,10 @@ import { AdminSidebarComponent as BaseComponent } from '../../../../../app/admin
  */
 @Component({
   selector: 'ds-themed-admin-sidebar',
-  // templateUrl: './admin-sidebar.component.html',
-  templateUrl: '../../../../../app/admin/admin-sidebar/admin-sidebar.component.html',
+  templateUrl: './admin-sidebar.component.html',
+  // templateUrl: '../../../../../app/admin/admin-sidebar/admin-sidebar.component.html',
   // styleUrls: ['./admin-sidebar.component.scss']
-  styleUrls: ['../../../../../app/admin/admin-sidebar/admin-sidebar.component.scss'],
+  styleUrls: ['./admin-sidebar.component.scss', '../../../../../app/admin/admin-sidebar/admin-sidebar.component.scss'],
   standalone: true,
   imports: [NgIf, NgbDropdownModule, NgClass, NgFor, NgComponentOutlet, AsyncPipe, TranslateModule],
 })
