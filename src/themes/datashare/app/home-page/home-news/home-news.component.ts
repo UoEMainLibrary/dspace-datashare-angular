6878f93d56e4440c73c049618bc6dd3365a30b82
John Pinto <jpinto@newington.edina.ac.uk>
2025-09-23 17:06:37 +0100

    Added the Datashare Home News page used in Dspace 8.1.

--- src/themes/datashare/app/home-page/home-news/home-news.component.ts
+++ src/themes/datashare/app/home-page/home-news/home-news.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 
 import { HomeNewsComponent as BaseComponent } from '../../../../../app/home-page/home-news/home-news.component';
+import { DepositButtonComponent } from '../../datashare/deposit-button/deposit-button.component';
+import { ThemedDepositButtonComponent } from '../../../../../app/datashare/deposit-button/themed-deposit-button.component';
 
 @Component({
   selector: 'ds-themed-home-news',
-  // styleUrls: ['./home-news.component.scss'],
-  styleUrls: ['../../../../../app/home-page/home-news/home-news.component.scss'],
-  // templateUrl: './home-news.component.html'
-  templateUrl: '../../../../../app/home-page/home-news/home-news.component.html',
+  styleUrls: ['./home-news.component.scss'],
+  // styleUrls: ['../../../../../app/home-page/home-news/home-news.component.scss'],
+  templateUrl: './home-news.component.html',
+  // templateUrl: '../../../../../app/home-page/home-news/home-news.component.html',
   standalone: true,
+  imports: [ThemedDepositButtonComponent],
 })
 
 /**
