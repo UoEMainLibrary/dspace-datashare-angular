ca59c4b15a9d18ef57c738860844614bd0d07059
John Pinto <jpinto@newington.edina.ac.uk>
2025-09-23 16:00:12 +0100

    Added code console log bitstreams for easy debugging.

--- src/app/item-page/simple/field-components/file-section/file-section.component.ts
+++ src/app/item-page/simple/field-components/file-section/file-section.component.ts
@@ -116,6 +116,12 @@ export class FileSectionComponent implements OnInit {
         this.notificationsService.error(this.translateService.get('file-section.error.header'), `${bitstreamsRD.statusCode} ${bitstreamsRD.errorMessage}`);
       } else if (hasValue(bitstreamsRD.payload)) {
         const current: Bitstream[] = this.bitstreams$.getValue();
+        // DATASHARE -start
+        // Console log bitstreams for easy debugging
+        bitstreamsRD.payload.page.forEach(bitstream => {
+          console.log('Bitstream:', bitstream);
+        });
+        // DATASHARE - end
         this.bitstreams$.next([...current, ...bitstreamsRD.payload.page]);
         this.isLoading = false;
         this.isLastPage = this.currentPage === bitstreamsRD.payload.totalPages;
