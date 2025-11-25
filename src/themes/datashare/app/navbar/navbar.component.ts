871abf744b79414921fb7ea4565af9f501e02f21
John Pinto <john.pinto@ed.ac.uk>
2025-11-25 12:57:43 +0000

    Added house-door-fill icon to Navbar component.

--- src/themes/datashare/app/navbar/navbar.component.ts
+++ src/themes/datashare/app/navbar/navbar.component.ts
@@ -12,19 +12,27 @@ import { ThemedUserMenuComponent } from 'src/app/shared/auth-nav-menu/user-menu/
 
 import { NavbarComponent as BaseComponent } from '../../../../app/navbar/navbar.component';
 import { slideMobileNav } from '../../../../app/shared/animations/slide';
+// DATASHARE - start
+import { RouterLink } from '@angular/router';
+// DATASHARE - end
 
 /**
  * Component representing the public navbar
  */
 @Component({
   selector: 'ds-themed-navbar',
-  // styleUrls: ['./navbar.component.scss'],
-  styleUrls: ['../../../../app/navbar/navbar.component.scss'],
-  // templateUrl: './navbar.component.html',
-  templateUrl: '../../../../app/navbar/navbar.component.html',
+  // DATASHARE - start
+  styleUrls: ['../../../../app/navbar/navbar.component.scss', './navbar.component.scss'],
+  // styleUrls: ['../../../../app/navbar/navbar.component.scss'],
+  templateUrl: './navbar.component.html',
+  // templateUrl: '../../../../app/navbar/navbar.component.html',
+  // DATASHARE - end
   animations: [slideMobileNav],
   standalone: true,
-  imports: [NgbDropdownModule, NgClass, NgIf, ThemedUserMenuComponent, NgFor, NgComponentOutlet, AsyncPipe, TranslateModule],
+  // DATASHARE - start
+  imports: [RouterLink, NgbDropdownModule, NgClass, NgIf, ThemedUserMenuComponent, NgFor, NgComponentOutlet, AsyncPipe, TranslateModule],
+  // imports: [NgbDropdownModule, NgClass, NgIf, ThemedUserMenuComponent, NgFor, NgComponentOutlet, AsyncPipe, TranslateModule],
+  // DATASHARE - end
 })
 export class NavbarComponent extends BaseComponent {
 }
