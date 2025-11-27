24f83b09739d0fca53ba92ba66141e162d1e9e7d
John Pinto <john.pinto@ed.ac.uk>
2025-11-27 18:01:55 +0000

    Added Upload files information as an alert to Submission form.

--- src/themes/datashare/app/submission/form/submission-upload-files/submission-upload-files.component.ts
+++ src/themes/datashare/app/submission/form/submission-upload-files/submission-upload-files.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
 import { UploaderComponent } from '../../../../../../app/shared/upload/uploader/uploader.component';
 import { SubmissionUploadFilesComponent as BaseComponent } from '../../../../../../app/submission/form/submission-upload-files/submission-upload-files.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { AlertComponent } from '../../../../../../app/shared/alert/alert.component';
+import { AlertType } from '../../../../../../app/shared/alert/alert-type';
+
 
 @Component({
   selector: 'ds-themed-submission-upload-files',
@@ -15,10 +18,18 @@ import { TranslateModule } from '@ngx-translate/core';
     UploaderComponent,
     NgIf,
     // DATASHARE - start
-    TranslateModule
+    AlertComponent,
+    TranslateModule,
     // DATASHARE - end
   ],
   standalone: true,
 })
 export class SubmissionUploadFilesComponent extends BaseComponent {
+  //  DATASHARE - start
+  /**
+   * The AlertType enumeration
+   * @type {AlertType}
+   */
+  public AlertTypeEnum = AlertType;
+  //  DATASHARE - end
 }
