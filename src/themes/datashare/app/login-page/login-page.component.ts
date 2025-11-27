0dceff0a93079b0e63a2ac8fefea435466ae6d76
John Pinto <john.pinto@ed.ac.uk>
2025-11-27 13:30:58 +0000

    Updated log in login with password page.

--- src/themes/datashare/app/login-page/login-page.component.ts
+++ src/themes/datashare/app/login-page/login-page.component.ts
@@ -9,10 +9,12 @@ import { LoginPageComponent as BaseComponent } from '../../../../app/login-page/
  */
 @Component({
   selector: 'ds-themed-login-page',
-  // styleUrls: ['./login-page.component.scss'],
-  styleUrls: ['../../../../app/login-page/login-page.component.scss'],
-  // templateUrl: './login-page.component.html'
-  templateUrl: '../../../../app/login-page/login-page.component.html',
+  // DATASHARE - start
+  styleUrls: ['./login-page.component.scss'],
+  // styleUrls: ['../../../../app/login-page/login-page.component.scss'],
+  templateUrl: './login-page.component.html',
+  //templateUrl: '../../../../app/login-page/login-page.component.html',
+  // DATASHARE - end
   standalone: true,
   imports: [ThemedLogInComponent, TranslateModule],
 })
