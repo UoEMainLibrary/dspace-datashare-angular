2a96810374968c1280ead4637e5af541e327664b
John Pinto <john.pinto@ed.ac.uk>
2025-11-27 17:12:50 +0000

    Added file upload message to submission form.

--- src/themes/datashare/app/submission/form/submission-upload-files/submission-upload-files.component.ts
+++ src/themes/datashare/app/submission/form/submission-upload-files/submission-upload-files.component.ts
@@ -3,14 +3,20 @@ import { Component } from '@angular/core';
 
 import { UploaderComponent } from '../../../../../../app/shared/upload/uploader/uploader.component';
 import { SubmissionUploadFilesComponent as BaseComponent } from '../../../../../../app/submission/form/submission-upload-files/submission-upload-files.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'ds-themed-submission-upload-files',
-  // templateUrl: './submission-upload-files.component.html',
-  templateUrl: '../../../../../../app/submission/form/submission-upload-files/submission-upload-files.component.html',
+  // DATASHARE - start
+  templateUrl: './submission-upload-files.component.html',
+  //templateUrl: '../../../../../../app/submission/form/submission-upload-files/submission-upload-files.component.html',
+  // DATASHARE - end
   imports: [
     UploaderComponent,
     NgIf,
+    // DATASHARE - start
+    TranslateModule
+    // DATASHARE - end
   ],
   standalone: true,
 })
