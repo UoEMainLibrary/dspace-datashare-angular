36202369eee5e9c5054d444b6cfb41f90523857e
milanmajchrak <milan.majchrak@dataquest.sk>
2026-04-08 14:14:25 +0200

    Fix lint: unused imports, trailing commas, import sort, wrong class name

--- src/app/datashare/deposit-button/deposit-button.component.ts
+++ src/app/datashare/deposit-button/deposit-button.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './deposit-button.component.html',
-  styleUrl: './deposit-button.component.scss'
+  styleUrl: './deposit-button.component.scss',
 })
 export class DepositButtonComponent {
 
